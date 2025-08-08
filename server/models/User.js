aaacdd2dd3adc71a9dcdf47fa7db09b590e86682
AleksJakub <124362847+AleksJakub@users.noreply.github.com>
2025-08-08 15:57:31 +0100

    feat: add role enum to user schema

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,11 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
-  role: { type: String, default: "user" },
+  role: {
+    type: String,
+    enum: ["Admin", "Manager", "Cleaner"],
+    default: "Cleaner",
+  },
   photoUrl: String,
 });
 
